8587fc25d33a65cc859a2c851e333500cb7e1183
Pedro Dobrescu <petre.dobrescu@gmail.com>
2026-05-14 10:50:45 +0300

    Update the GitHub link for GitUpdater

--- resmushit.php
+++ resmushit.php
@@ -17,7 +17,7 @@
  * Author:            Charles Bourgeaux, ShortPixel
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * GitHub Plugin URI: https://github.com/resmushit/wordpress-plugin
+ * GitHub Plugin URI: https://github.com/short-pixel-optimizer/resmushit-image-optimizer
  * Domain Path:       /languages
  * Text Domain:	      resmushit-image-optimizer
  */
